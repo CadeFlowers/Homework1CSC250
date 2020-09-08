7668870b9e4520e8843cb72aa86c1b84a52d9a9b
CadeFlowers <70229600+CadeFlowers@users.noreply.github.com>
2020-09-07 22:44:02 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,17 @@
-public class Driver {
+public class Driver 
+{
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!!");
+		String s = "Acts";
+		int count = 0;
+		for(int i = s.length()-1; i >= 0; i--)
+		{
+			if(s.charAt(i) == 'a' || s.charAt(i) == 'A' || s.charAt(i) == 'e' || s.charAt(i) == 'E' || s.charAt(i) == 'i' || s.charAt(i) == 'I' || s.charAt(i) == 'o' || s.charAt(i) == 'O' || s.charAt(i) == 'u' || s.charAt(i) == 'U' || s.charAt(i) == 'y' || s.charAt(i) == 'Y')
+			{
+				count++;
+			}
+				
+		}
+		System.out.println(count);	
 	}
-
 }
